d36fa84ffbc98fc8f505b359f602cd7a7e2bdc64
sanghyun-enerzai <122770521+sanghyun-enerzai@users.noreply.github.com>
2023-03-10 20:59:27 +0900

    import multi images

--- annotator.py
+++ annotator.py
@@ -1,7 +1,9 @@
 import cv2
 import tkinter as tk
+import tkinter.messagebox as messagebox
 import numpy as np
 
+from tkinter import ttk
 from pathlib import Path
 from tkinter import filedialog
 from PIL import Image, ImageTk
@@ -13,58 +15,96 @@ class SegmentationAnnotator(tk.Tk):
         self.title('Segmentation Annotator')
         self.geometry('800x600+100+100')
 
+        tool = tk.Frame(self)
+        tool.pack(side='top', anchor='n', fill='x')
+
         # loader frame
-        loader = tk.Frame(self)
-        loader.pack(side='top', fill='x', expand=False)
+        loader = tk.Frame(tool)
+        loader.pack(side='top', fill='x', expand=True)
 
         # image loader
         image_loader = tk.Frame(loader)
-        image_loader.pack(side='top', fill='both', expand=True)
+        image_loader.pack(side='left', fill='x', expand=True)
 
-        image_loader_label = tk.Label(image_loader, text='Load Image File: ')
-        image_loader_label.pack(side='left')
+        image_finder = tk.Frame(image_loader)
+        image_finder.pack(side='top', fill='x', expand=True)
 
-        self.image_loader_entry = tk.Entry(image_loader, takefocus=0, state='readonly')
-        self.image_loader_entry.pack(side='left', fill='x', expand=True)
+        image_loader_label = tk.Label(image_finder, text='Image Files')
+        image_loader_label.pack(side='left', fill='x', expand=True)
 
-        image_loader_button = tk.Button(image_loader, text='Find', command=self.image_filedialog)
+        image_loader_button = tk.Button(image_finder, text='Find', command=self.image_filedialog)
         image_loader_button.pack(side='right')
+
+        image_list = tk.Frame(image_loader)
+        image_list.pack(side='top', fill='x', expand=True)
+
+        self.image_loader_listbox = tk.Listbox(image_list, selectmode='extended', height=6)
+        self.image_loader_listbox.pack(side='left', fill='x', expand=True)
+        self.image_loader_listbox.bind('<Delete>', self.delete_image_from_loader)
+
+        image_loader_scroll = tk.Scrollbar(image_list)
+        image_loader_scroll.pack(side='right', fill='y')
+
+        self.image_loader_listbox.config(yscrollcommand=image_loader_scroll.set)
+        image_loader_scroll.config(command=self.image_loader_listbox.yview)
+
+        annot_selector = tk.Frame(tool)
+        annot_selector.pack(side='top', fill='x')
+
+        annot_selector_left = tk.Button(annot_selector, text='(Page Up) <--', command=self.go_prev_image)
+        annot_selector_left.pack(side='left')
+        self.bind('<Prior>', lambda e: self.go_prev_image())
+
+        self.annot_selector_filename_label = tk.Label(annot_selector, text='')
+        self.annot_selector_filename_label.pack(side='left', fill='x', expand=True)
+
+        self.annot_selector_number_label = tk.Label(annot_selector, text='(0/0)')
+        self.annot_selector_number_label.pack(side='left', fill='x', expand=True)
+
+        annot_selector_right = tk.Button(annot_selector, text='--> (Page Down)', command=self.go_next_image)
+        annot_selector_right.pack(side='right')
+        self.bind('<Next>', lambda e: self.go_next_image())
 
         # annotation loader
         annot_loader = tk.Frame(loader)
-        annot_loader.pack(side='top', fill='both', expand=True)
+        annot_loader.pack(side='left', fill='x', expand=True)
 
-        annot_loader_label = tk.Label(annot_loader, text='Load Annotation File: ')
-        annot_loader_label.pack(side='left')
+        annot_finder = tk.Frame(annot_loader)
+        annot_finder.pack(side='top', fill='x', expand=True)
 
-        self.annot_loader_entry = tk.Entry(annot_loader, takefocus=0, state='readonly')
-        self.annot_loader_entry.pack(side='left', fill='x', expand=True)
+        annot_loader_label = tk.Label(annot_finder, text='Annotation Files')
+        annot_loader_label.pack(side='left', fill='x', expand=True)
 
-        annot_loader_button = tk.Button(annot_loader, text='Find', command=self.annot_filedialog)
+        annot_loader_button = tk.Button(annot_finder, text='Find', command=self.annot_filedialog)
         annot_loader_button.pack(side='right')
 
-        # start and save frame
-        buttons = tk.Frame(loader)
-        buttons.pack(side='top', fill='x', expand=True)
+        annot_list = tk.Frame(annot_loader)
+        annot_list.pack(side='top', fill='x', expand=True)
+
+        self.annot_loader_listbox = tk.Listbox(annot_list, selectmode='extended', height=6)
+        self.annot_loader_listbox.pack(side='left', fill='x', expand=True)
+        self.annot_loader_listbox.bind('<Delete>', self.delete_annot_from_loader)
+
+        annot_loader_scroll = tk.Scrollbar(annot_list)
+        annot_loader_scroll.pack(side='right', fill='y')
+
+        self.annot_loader_listbox.config(yscrollcommand=annot_loader_scroll.set)
+        annot_loader_scroll.config(command=self.annot_loader_listbox.yview)
 
         # start annotation button
-        start_button = tk.Button(buttons, text='Start Annotation', command=self.start_annotation)
-        start_button.pack(side='left', fill='x', expand=True)
+        start_button = tk.Button(loader, text='Start Annotation', command=self.start_annotation)
+        start_button.pack(side='right', fill='y')
 
-        # save annotation button
-        self.annot_save_button = tk.Button(buttons, text='Save Annotation (Crtl+S)', command=self.save_annotation, state='disabled')
-        self.annot_save_button.pack(side='right', fill='x', expand=True)
-
-        self.image_path = None
-        self.annot_path = None
+        self.image_paths = []
+        self.annot_paths = []
         self.cv_image = None
-        self.cv_annot = None
+        self.cv_annots = []
         self.original_canvas_image = None
         self.loaded = False
 
         # draw and erase mode
-        mode_frame = tk.Frame(loader)
-        mode_frame.pack(side='top', fill='x', expand=True)
+        mode_frame = tk.Frame(tool, background='blue')
+        mode_frame.pack(side='top', fill='x')
 
         self.is_draw = tk.IntVar(self, value=1)
         self.mode_draw = tk.Radiobutton(mode_frame, text='Draw Label (1)', value=1, variable=self.is_draw)
@@ -96,41 +136,86 @@ class SegmentationAnnotator(tk.Tk):
         self.canvas_lines = []
         self.created_annots = []
         self.removed_annots = []
+        self.total = 0
+        self.current = 0
         
     def image_filedialog(self):
-        if self.image_path is None:
-            path = filedialog.askopenfilename(filetypes=[('Image File', '.jpg'), ('Image File', '.png')])
+        if len(self.image_paths) == 0:
+            paths = filedialog.askopenfilename(multiple=True, filetypes=[('Image File', '.jpg'), ('Image File', '.png')])
         else:
-            path = filedialog.askopenfilename(initialdir=self.image_path.parent, filetypes=[('Image File', '.jpg'), ('Image File', '.png')])
-        if path == '':
+            paths = filedialog.askopenfilename(multiple=True, initialdir=self.image_paths[0].parent, filetypes=[('Image File', '.jpg'), ('Image File', '.png')])
+        if paths == '':
             return
-        self.image_loader_entry.config(state='normal')
-        self.image_loader_entry.delete(0, tk.END)
-        self.image_loader_entry.insert(0, path)
-        self.image_loader_entry.config(state='readonly')
+        self.image_paths = sorted([Path(path) for path in paths])
+        self.image_loader_listbox.delete(0, tk.END)
+        for i in range(len(self.image_paths)):
+            self.image_loader_listbox.insert(i, self.image_paths[i].parent.name + '/' + self.image_paths[i].name)
 
     def annot_filedialog(self):
-        if self.annot_path is None:
-            path = filedialog.askopenfilename(filetypes=[('Image File', '.png')])
+        if len(self.annot_paths) == 0:
+            paths = filedialog.askopenfilename(multiple=True, filetypes=[('Image File', '.png')])
         else:
-            path = filedialog.askopenfilename(initialdir=self.annot_path.parent, filetypes=[('Image File', '.png')])
-        if path == '':
+            paths = filedialog.askopenfilename(multiple=True, initialdir=self.annot_paths[0].parent, filetypes=[('Image File', '.png')])
+        if paths == '':
             return
-        self.annot_loader_entry.config(state='normal')
-        self.annot_loader_entry.delete(0, tk.END)
-        self.annot_loader_entry.insert(0, path)
-        self.annot_loader_entry.config(state='readonly')
+        self.annot_paths = sorted([Path(path) for path in paths])
+        self.annot_loader_listbox.delete(0, tk.END)
+        for i in range(len(self.annot_paths)):
+            self.annot_loader_listbox.insert(i, self.annot_paths[i].parent.name + '/' + self.annot_paths[i].name)
 
     def start_annotation(self):
-        self.annot_canvas.delete('all')
-        self.image_path = Path(self.image_loader_entry.get())
-        self.annot_path = Path(self.annot_loader_entry.get())
+        if len(self.image_paths) != len(self.annot_paths):
+            messagebox.showerror('Error', 'Number of Image Files != Number of Annotation Files')
+            return
+        
+        valid_flag = True
+        for i in range(len(self.image_paths)):
+            image_name = self.image_paths[i].name
+            annot_name = self.annot_paths[i].name
+            if image_name[:-4] != annot_name[:-4]:
+                valid_flag = False
+            break
+        if not valid_flag:
+            messagebox.showerror('Error', 'Name of Image File != Name of Annotation File')
+            return
 
-        self.cv_image = np.array(Image.open(str(self.image_path)).convert('RGB'))[:, :, ::-1]
-        self.cv_image = np.ascontiguousarray(self.cv_image, dtype=np.uint8)
-        self.cv_annot = np.array(Image.open(str(self.annot_path)).convert('RGB'))[:, :, ::-1]
-        self.cv_annot = np.ascontiguousarray(self.cv_annot, dtype=np.uint8)
-        cv_annot_gray = cv2.cvtColor(self.cv_annot, cv2.COLOR_BGR2GRAY)
+        self.total = len(self.image_paths)
+        if self.total == 0:
+            self.current = 0
+            self.annot_selector_filename_label.config(text='')
+            self.annot_selector_number_label.config(text=f'0/0')
+            return
+        
+        self.loaded = True
+        self.current = 0
+        self.cv_annots = [np.ascontiguousarray(np.array(Image.open(str(annot_path)).convert('RGB'))[:, :, ::-1], dtype=np.uint8) for annot_path in self.annot_paths]
+        self.load_new_image()
+
+        self.annot_canvas.bind('<Button3-Motion>', self.move_canvas)
+        self.annot_canvas.bind('<ButtonRelease-3>', self.release_canvas)
+        self.annot_canvas.bind('<Button-1>', self.add_point)
+        self.bind('<Return>', self.make_polygon)
+        self.bind('<space>', self.make_polygon)
+        self.bind('<Control-z>', self.remove_last_point)
+        self.bind('<Control-Z>', self.remove_last_point)
+        self.bind('<Control-s>', lambda e: self.save_annotation())
+        self.bind('<Control-S>', lambda e: self.save_annotation())
+        self.bind('<KeyPress-q>', self.change_to_original)
+        self.bind('<KeyPress-Q>', self.change_to_original)
+        self.bind('<KeyRelease-q>', self.change_to_overlay)
+        self.bind('<KeyRelease-Q>', self.change_to_overlay)
+        self.bind('<KeyPress-Left>', self.go_back_annot)
+        self.bind('<KeyPress-Right>', self.go_front_annot)
+        self.loaded = True
+
+    def load_new_image(self):
+        self.annot_canvas.delete('all')
+        self.annot_selector_filename_label.config(text=f'{self.image_paths[self.current].name}')
+        self.annot_selector_number_label.config(text=f'{self.current + 1}/{self.total}')
+        self.title(f'Segmentation Annotator - {self.annot_paths[self.current].name}')
+        
+        self.cv_image = np.array(Image.open(str(self.image_paths[self.current])).convert('RGB'))[:, :, ::-1]
+        cv_annot_gray = cv2.cvtColor(self.cv_annots[self.current], cv2.COLOR_BGR2GRAY)
         self.h, self.w, c = self.cv_image.shape
         self.color_map = np.zeros((self.h, self.w, 3), dtype=np.uint8)
         for i in range(3):
@@ -149,32 +234,11 @@ class SegmentationAnnotator(tk.Tk):
         self.canvas_xpos, self.canvas_ypos = self.w // 2, self.h // 2
         self.canvas_image_obj = self.annot_canvas.create_image(self.canvas_xpos, self.canvas_ypos, image=self.canvas_image)
 
-        self.annot_canvas.bind('<Button3-Motion>', self.move_canvas)
-        self.annot_canvas.bind('<ButtonRelease-3>', self.release_canvas)
-        self.annot_canvas.bind('<Button-1>', self.add_point)
-        self.bind('<Return>', self.make_polygon)
-        self.bind('<space>', self.make_polygon)
-        self.bind('<Control-z>', self.remove_last_point)
-        self.bind('<Control-Z>', self.remove_last_point)
-        self.bind('<Control-s>', lambda e: self.save_annotation())
-        self.bind('<Control-S>', lambda e: self.save_annotation())
-        self.bind('<KeyPress-q>', self.change_to_original)
-        self.bind('<KeyPress-Q>', self.change_to_original)
-        self.bind('<KeyRelease-q>', self.change_to_overlay)
-        self.bind('<KeyRelease-Q>', self.change_to_overlay)
-        self.bind('<KeyPress-Left>', self.go_back_annot)
-        self.bind('<KeyPress-Right>', self.go_front_annot)
-        
-        self.annot_save_button.config(state='normal')
-        self.loaded = True
-
         self.points = []
         self.canvas_points = []
         self.canvas_lines = []
-        self.created_annots = [self.cv_annot.copy()]
+        self.created_annots = [self.cv_annots[self.current].copy()]
         self.removed_annots = []
-
-        self.title(f'Segmentation Annotator - {self.image_path.name} | {self.annot_path.name}')
 
     def move_canvas(self, event):
         if self.move_canvas_flag:
@@ -233,10 +297,10 @@ class SegmentationAnnotator(tk.Tk):
 
         polygon = np.array(self.points).reshape(-1, 1, 2).astype(np.int32)
         if self.is_draw.get() == 1:
-            self.cv_annot = cv2.drawContours(self.cv_annot, [polygon], -1, color=(255, 255, 255), thickness=-1)
+            self.cv_annots[self.current] = cv2.drawContours(self.cv_annots[self.current], [polygon], -1, color=(255, 255, 255), thickness=-1)
         else:
-            self.cv_annot = cv2.drawContours(self.cv_annot, [polygon], -1, color=(0, 0, 0), thickness=-1)
-        cv_annot_gray = cv2.cvtColor(self.cv_annot, cv2.COLOR_BGR2GRAY)
+            self.cv_annots[self.current] = cv2.drawContours(self.cv_annots[self.current], [polygon], -1, color=(0, 0, 0), thickness=-1)
+        cv_annot_gray = cv2.cvtColor(self.cv_annots[self.current], cv2.COLOR_BGR2GRAY)
         self.cv_overlay = self.cv_image.copy()
         self.cv_overlay[cv_annot_gray > 0, :] = (1 - self.opacity) * self.cv_image[cv_annot_gray > 0, :] + self.opacity * self.color_map[cv_annot_gray > 0, :]
 
@@ -254,7 +318,7 @@ class SegmentationAnnotator(tk.Tk):
         self.canvas_points = []
         self.canvas_lines = []
 
-        self.created_annots.append(self.cv_annot.copy())
+        self.created_annots.append(self.cv_annots[self.current].copy())
         self.removed_annots = []
 
     def remove_last_point(self, event):
@@ -267,11 +331,10 @@ class SegmentationAnnotator(tk.Tk):
             self.annot_canvas.delete(self.canvas_lines.pop())
 
     def save_annotation(self):
-        path = filedialog.asksaveasfilename(initialfile=self.annot_path.name, filetypes=[('Image File', '.png')])
-        if path[-4:] != '.png':
-            path += '.png'
-        cv_annot_img = Image.fromarray(self.cv_annot)
-        cv_annot_img.save(path)
+        path = Path(filedialog.askdirectory(initialdir=self.annot_paths[0].parent, mustexist=True))
+        for i in range(self.total):
+            cv_annot_img = Image.fromarray(self.cv_annots[i])
+            cv_annot_img.save(path / self.annot_paths[i].name)
 
     def change_to_original(self, event):
         if not self.loaded:
@@ -286,9 +349,9 @@ class SegmentationAnnotator(tk.Tk):
     def go_back_annot(self, event):
         if len(self.created_annots) > 1:
             self.removed_annots.append(self.created_annots.pop())
-            self.cv_annot = self.created_annots[-1].copy()
+            self.cv_annots[self.current] = self.created_annots[-1].copy()
             
-            cv_annot_gray = cv2.cvtColor(self.cv_annot, cv2.COLOR_BGR2GRAY)
+            cv_annot_gray = cv2.cvtColor(self.cv_annots[self.current], cv2.COLOR_BGR2GRAY)
             self.cv_overlay = self.cv_image.copy()
             self.cv_overlay[cv_annot_gray > 0, :] = (1 - self.opacity) * self.cv_image[cv_annot_gray > 0, :] + self.opacity * self.color_map[cv_annot_gray > 0, :]
             self.cv_overlay = cv2.cvtColor(self.cv_overlay, cv2.COLOR_BGR2RGB)
@@ -298,16 +361,38 @@ class SegmentationAnnotator(tk.Tk):
 
     def go_front_annot(self, event):
         if len(self.removed_annots) > 0:
-            self.cv_annot = self.removed_annots.pop()
-            self.created_annots.append(self.cv_annot.copy())
+            self.cv_annots[self.current] = self.removed_annots.pop()
+            self.created_annots.append(self.cv_annots[self.current].copy())
 
-            cv_annot_gray = cv2.cvtColor(self.cv_annot, cv2.COLOR_BGR2GRAY)
+            cv_annot_gray = cv2.cvtColor(self.cv_annots[self.current], cv2.COLOR_BGR2GRAY)
             self.cv_overlay = self.cv_image.copy()
             self.cv_overlay[cv_annot_gray > 0, :] = (1 - self.opacity) * self.cv_image[cv_annot_gray > 0, :] + self.opacity * self.color_map[cv_annot_gray > 0, :]
             self.cv_overlay = cv2.cvtColor(self.cv_overlay, cv2.COLOR_BGR2RGB)
             self.cv_overlay = Image.fromarray(self.cv_overlay)
             self.canvas_image = ImageTk.PhotoImage(image=self.cv_overlay)
             self.annot_canvas.itemconfig(self.canvas_image_obj, image=self.canvas_image)
+
+    def delete_image_from_loader(self, event):
+        selected = sorted(list(self.image_loader_listbox.curselection()), reverse=True)
+        for idx in selected:
+            self.image_paths.pop(idx)
+            self.image_loader_listbox.delete(idx)
+
+    def delete_annot_from_loader(self, event):
+        selected = sorted(list(self.annot_loader_listbox.curselection()), reverse=True)
+        for idx in selected:
+            self.annot_paths.pop(idx)
+            self.annot_loader_listbox.delete(idx)
+
+    def go_prev_image(self):
+        if self.current > 0:
+            self.current -= 1
+            self.load_new_image()
+    
+    def go_next_image(self):
+        if self.current + 1 < self.total:
+            self.current += 1
+            self.load_new_image()
 
 
 if __name__ == '__main__':
